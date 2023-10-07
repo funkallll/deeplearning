6a7b36592140c83e7c4b87f5a11ace4e0deccb3d
funkallll <116174286+funkallll@users.noreply.github.com>
2023-10-07 09:59:19 +0800

    Update rgb_gray.py

--- rgb_gray.py
+++ rgb_gray.py
@@ -1,17 +1,25 @@
 import os
 import cv2
-import numpy as np
 
-def convert2gray(filename):                                     # 将彩色图转灰度图的函数
-    img = cv2.imread(file_path+'/'+filename, 1)                 # 1是以彩色图方式去读
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite(out_path + '/GRAY' + filename, gray_img)        # 保存在新文件夹下，且图名中加GRAY
+# 输入和输出文件夹路径
+input_folder = 'photos'
+output_folder = 'grays'
 
-file_path = "F:/pycharmpractice/my/datazip/ORCA-Uboat_data/WaterSegmentation/training/training/640_320_undistorted_gt"               # 输入文件夹
-#os.mkdir("F:/pycharmpractice/my/datazip/ORCA-Uboat_data/WaterSegmentation/training/training/640_320_undistorted_gt_gray")            # 建立新的目录
-out_path ="F:/pycharmpractice/my/datazip/ORCA-Uboat_data/WaterSegmentation/training/training/640_320_undistorted_gt_gray"            # 设置为新目录为输出文件夹
+# 确保输出文件夹存在
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
-for filename in os.listdir(file_path):                          # 遍历输入路径，得到图片名
-    print(filename)
-    convert2gray(filename)
+# 遍历输入文件夹中的所有图像
+for filename in os.listdir(input_folder):
+    if filename.endswith('.jpg') or filename.endswith('.png'):
+        # 读取图像
+        img = cv2.imread(os.path.join(input_folder, filename))
 
+        # 将图像转换为灰度图像
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # 使用阈值将灰度图像转换为二值图像
+        _, binary_img = cv2.threshold(gray_img, 127, 255, cv2.THRESH_BINARY)
+
+        # 保存二值图像到输出文件夹
+        cv2.imwrite(os.path.join(output_folder, filename), binary_img)
